46ac4cc025020800f923742ce7f888fa7020d483
Ryan-Habets <507646@vistacollege.nl>
2022-06-15 15:11:59 +0200

    upload

---
+++ background.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="background.css">
+</head>
+<body>
+    
+</body>
+</html>
+
+<div class="container">
+
+    <div class='plant1'>
+        <img class="plant1-1" src="./images/2.png" alt="plant1-1">
+    </div>
+    <div class='plant2'>
+        <img class="plant2-2" src="./images/6.png" alt="plant2-2">
+    </div>
+    <div class='plant3'>
+        <img class="plant3-3" src="./images/4.png" alt="plant3-3">
+    </div>
+    <div class='plant4'>
+
+    </div>
+    <div class='plant5'>
+
+    </div>
+    <div class='plant6'>
+
+    </div>
+
+	
+	
+	
+	<div class="bird-container bird-container--one">
+		<div class="bird bird--one"></div>
+	</div>
+	
+	<div class="bird-container bird-container--two">
+		<div class="bird bird--two"></div>
+	</div>
+	
+	<div class="bird-container bird-container--three">
+		<div class="bird bird--three"></div>
+	</div>
+	
+	<div class="bird-container bird-container--four">
+		<div class="bird bird--four"></div>
+	</div>
+	
+</div>
