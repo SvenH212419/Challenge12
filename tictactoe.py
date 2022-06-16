8b7b68c23eb3a2cb6cf6403a3bd19ad1d16aa2ec
sven1719 <212419@vistacollege.nl>
2022-06-16 11:00:43 +0200

    bug fix line width

--- tictactoe.py
+++ tictactoe.py
@@ -6,6 +6,7 @@ pygame. init()
 
 WIDTH = 600
 HEIGHT = 600
+LINE_WIDTH = 15
 # rgb: red green blue
 RED = (255, 0, 0)
 BG_COLOR = (28, 170, 156)
@@ -19,21 +20,21 @@ screen.fill( BG_COLOR )
 
 def draw_lines():
     # 1 horizontal
-    pygame.draw.line( screen, Line_COLOR, (0, 200), (600,200), LINE_WIDTH   )
+    pygame.draw.line( screen, Line_COLOR, (0, 200), (600,200), LINE_WIDTH  )
     # 2 horizontal
-    pygame.draw.line( screen, Line_COLOR, (0, 400), (600,400), LINE_WIDTH   )
+    pygame.draw.line( screen, Line_COLOR, (0, 400), (600,400), LINE_WIDTH  )
    
     # 1 vertical
-    pygame.draw.line( screen, Line_COLOR, (200, 0), (200,600), LINE_WIDTH   )
+    pygame.draw.line( screen, Line_COLOR, (200, 0), (200,600), LINE_WIDTH  )
     # 2 vertical
-    pygame.draw.line( screen, Line_COLOR, (400, 0), (400,600), LINE_WIDTH   )
+    pygame.draw.line( screen, Line_COLOR, (400, 0), (400,600), LINE_WIDTH  )
 
 draw_lines()
 
 # mainloop
 while True:
-      for event in pygame.event.get():
-          if event.type == pygame.QUIT:
-              sys.exit()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
 
-       pygame.display.update()
+    pygame.display.update()
