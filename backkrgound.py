83b6fc60a6ea720f95b36d7ca53446e4e3811817
Ryan-Habets <507646@vistacollege.nl>
2022-06-20 14:07:04 +0200

    upload

--- backkrgound.py
+++ backkrgound.py
@@ -12,7 +12,23 @@ pygame.display.set_caption( 'TIC TAC TOE')
 screen.fill( RED )
 
 Plantimg1 = pygame.image.load('./images/2.png')
-
+Plantimg2 = pygame.image.load('./images/6.png')
+Plantimg2 = pygame.transform.smoothscale(Plantimg2, (700, 600)) 
+Plantimg2 = pygame.transform.rotate(Plantimg2, -23)
+Plantimg3 = pygame.image.load('./images/4.png')
+Plantimg3 = pygame.transform.smoothscale(Plantimg3, (650, 550))
+Plantimg3 = pygame.transform.rotate(Plantimg3, (-70 ))
+Plantimg4 = pygame.image.load('./images/5.png')
+Plantimg4 = pygame.transform.smoothscale(Plantimg4, (650, 550))
+Plantimg4 = pygame.transform.flip(Plantimg4, True, False)
+Plantimg5 = pygame.image.load('./images/1.png')
+Plantimg5 = pygame.transform.smoothscale(Plantimg5, (550, 450))
+Plantimg6 = pygame.image.load('./images/3.png')
+Plantimg6 = pygame.transform.smoothscale(Plantimg6, (550,450))
+Plantimg6 = pygame.transform.rotate(Plantimg6, (-30))
+Plantimg7 = pygame.image.load('./images/Naamloos.png')
+Plantimg7 = pygame.transform.smoothscale(Plantimg7, (550, 450))
+Plantimg7 = pygame.transform.rotate(Plantimg7, (-90))
 # mainloop
 while True:
     for event in pygame.event.get():
@@ -22,3 +38,11 @@ while True:
     pygame.display.update()       
 
     screen.blit(Plantimg1, (-200, -200)) 
+    screen.blit(Plantimg3, (-200, 200))
+    screen.blit(Plantimg2, (-290, -20 ))
+    screen.blit(Plantimg5, (1120, 100))
+    screen.blit(Plantimg4, (1070, -100))
+    screen.blit(Plantimg7, (1200, 380))
+    screen.blit(Plantimg6, (1100, 110))
+    
+   
--- tictactoe.py
+++ tictactoe.py
@@ -1,3 +1,4 @@
+from inspect import modulesbyfile
 from site import USER_SITE
 from turtle import width
 from xml.dom import UserDataHandler
@@ -11,10 +12,16 @@ HEIGHT = 600
 LINE_WIDTH = 15
 BOARD_ROWS = 3
 BOARD_COLS = 3
+CIRCLE_RADIUS = 60
+CIRCLE_WIDTH = 15
+CROSS_WIDTH = 25
+SPACE = 55
 # rgb: red green blue
 RED = (255, 0, 0)
 BG_COLOR = (28, 170, 156)
 Line_COLOR = (23, 145, 135)
+CIRCLE_COLOR = (239, 231, 200)
+CROSS_COLOR = (66, 66, 66)
 
 screen = pygame.display.set_mode( (WIDTH, HEIGHT) )
 pygame.display.set_caption( 'TIC TAC TOE' )
@@ -36,6 +43,14 @@ def draw_lines():
     # 2 vertical
     pygame.draw.line( screen, Line_COLOR, (400, 0), (400,600), LINE_WIDTH  )
 
+def draw_figures():
+    for row in range(BOARD_ROWS):
+        for col in range(BOARD_COLS):
+            if board[row][col] == 1:
+                pygame.draw.circle( screen, CIRCLE_COLOR, (int( col * 200 + 100 ), int( row * 200 + 100 )), CIRCLE_RADIUS, CIRCLE_WIDTH )
+            elif board[row] [col] == 2:
+                pygame.draw.line( screen, CROSS_COLOR, (col * 200 + SPACE, row * 200 + 200 - SPACE), (col * 200 + 200 - SPACE, row * 200 + SPACE), CROSS_WIDTH )
+                pygame.draw.line( screen, CROSS_COLOR, (col * 200 + SPACE, row * 200 + SPACE), (col * 200 + 200 - SPACE, row * 200 + 200 - SPACE), CROSS_WIDTH  )
 def mark_square(row, col, player):
     board[row][col] = player 
 
@@ -51,22 +66,38 @@ def is_board_full():
     return True
 
 
-# false
-print(is_board_full())
-# marking all squares
-for row in range(BOARD_ROWS):
-        for col in range(BOARD_COLS):
-            mark_square( row, col, 1 )
-# board is full - true             
- print(is_board_full())            
-
 draw_lines()
+
+player = 1
 
 # mainloop
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+
+            MouseX = event.pos[0] # x
+            MouseY = event.pos[1] # y
+
+            clicked_row=int(MouseY // 200)
+            clicked_col=int(MouseX // 200)
+
+
+            if available_square( clicked_row, clicked_col ):
+                if player == 1:
+                    mark_square( clicked_row, clicked_col, 1)
+                    player = 2
+
+                elif player == 2:
+                    mark_square( clicked_row, clicked_col, 2)
+                    player = 1
+
+                draw_figures()
+
+
+
 
     pygame.display.update()
     
